ed1449163d5e3d7149a5c9e152f84642df28b5d7
qpeterp <lsepeter0827@naver.com>
2024-10-09 21:36:41 +0900

    first commit

---
+++ fitbattledesignsystem/src/main/java/com/qpeterp/fitbattledesignsystem/theme/FitBattleColor.kt
@@ -0,0 +1,38 @@
+package com.qpeterp.fitbattledesignsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+/*
+* 운동 한판 컬러를 정의한다는 뜻
+* */
+object FitBattleColor {
+    val White = Color(0xFFFFFFFF)
+    val Black = Color(0xFF000000)
+    val Background = Color(0xFFFAFAFA)
+
+    val MainColor = Color(0xFFF06543)
+
+    val MainColor50 = Color(0xFFFFE3DC)
+    val MainColor100 = Color(0xFFFFC6B8)
+    val MainColor200 = Color(0xFFFFA794)
+    val MainColor300 = Color(0xFFFF896F)
+    val MainColor400 = Color(0xFFFF6C4A)
+    val MainColor500 = Color(0xFFF06543)
+    val MainColor600 = Color(0xFFD95A3B)
+    val MainColor700 = Color(0xFFBF4F33)
+    val MainColor800 = Color(0xFFA5452B)
+    val MainColor900 = Color(0xFF8B3A23)
+
+    val Gray50 = Color(0xFFEEEEF1)
+    val Gray100 = Color(0xFFD2D2DB)
+    val Gray200 = Color(0xFFB5B5C4)
+    val Gray300 = Color(0xFF9999AD)
+    val Gray400 = Color(0xFF7D7D97)
+    val Gray500 = Color(0xFF64647D)
+    val Gray600 = Color(0xFF4D4D60)
+    val Gray700 = Color(0xFF363644)
+    val Gray800 = Color(0xFF202028)
+    val Gray900 = Color(0xFF09090B)
+
+}
